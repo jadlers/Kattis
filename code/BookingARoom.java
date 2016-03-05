a4b131ff3d70b4f64fe4c5fe8104a167dedecd6f
Jacob Adlers <jadlers@kth.se>
2016-03-05 19:58:42 +0100

    Solved Booking a Room

---
+++ code/BookingARoom.java
@@ -0,0 +1,29 @@
+/**
+ * Solution for problem: https://open.kattis.com/problems/bookingaroom
+ * Created by jadlers on 2016-03-05.
+ * Running time: 0.12s
+ */
+public class BookingARoom {
+	public static void main(String[] args) {
+		Kattio io = new Kattio(System.in, System.out);
+		int numberOfRooms = io.getInt();
+		boolean[] rooms = new boolean[numberOfRooms];
+		int bookedRooms = io.getInt();
+		for (int i = 0; i < bookedRooms; i++) {
+			rooms[io.getInt() - 1] = true;
+		}
+
+		boolean printed = false;
+		for (int i = 0; i < rooms.length; i++) {
+			if (!rooms[i]) {
+				io.println(i+1);
+				printed = true;
+				break;
+			}
+		}
+		if (!printed) {
+			io.println("too late");
+		}
+		io.flush();
+	}
+}
